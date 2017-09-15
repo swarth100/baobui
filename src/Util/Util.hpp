f98e6ec2533da32225551eec412fdf3c79a0d633
Alberto Spina <as12015@ic.ac.uk>
2017-09-15 10:24:05 +0200

    Util/Util.hpp: Refactor Util to contain most import statements

--- src/Util/Util.hpp
+++ src/Util/Util.hpp
@@ -1,6 +1,13 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <GL/glew.h>		// include GLEW and new version of GL on Windows
+#include <GLFW/glfw3.h> // GLFW helper library
+#include <assert.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <stdexcept>
 #include <iostream>
 #include <fstream>
