afd4d83a5461fec8bac187aa75c5a7c48f1c255a
Alberto Spina <as12015@ic.ac.uk>
2017-09-16 09:31:09 +0200

    VboElement.cpp: Refactor adding comments over methods and class

--- src/Geometry/VboElement.cpp
+++ src/Geometry/VboElement.cpp
@@ -1,18 +1,26 @@
 #include "VboElement.hpp"
 
+/* --------------------------------- Public --------------------------------- */
+
+/* A VboElement is initialised giving it an array of GLfloats and its size. */
 VboElement::VboElement(GLfloat* array, int size) {
+  /* The vbo field is initialised and bound as a buffer */
   glGenBuffers(1, &this->vbo);
   glBindBuffer(GL_ARRAY_BUFFER, this->vbo);
 
+  /* The input array is bound to the new vbo buffer for the given size */
   glBufferData(GL_ARRAY_BUFFER, size * 3 * sizeof(GLfloat), array, GL_STATIC_DRAW);
 
+  /* The size is stored as a reference */
   this->vboSize = size;
 }
 
+/* VBO Buffer handler getter */
 GLuint VboElement::getVbo() {
   return this->vbo;
 }
 
+/* Vbo size getter */
 int VboElement::getVboSize() {
   return this->vboSize;
 }
--- src/Geometry/VboElement.hpp
+++ src/Geometry/VboElement.hpp
@@ -3,20 +3,21 @@
 
 #include "../Util/Util.hpp"
 
+/* A VboElement stores buffered VBO array data together with it size.
+   VboElements should be accessed whenever they need to be linked into a Vao. */
 class VboElement {
-
 public:
+  /* Constructor */
   VboElement(GLfloat* array, int size);
 
+  /* Getters for private fields */
   GLuint getVbo();
   int getVboSize();
 
 private:
+  /* Private fields */
   GLuint vbo;
   int vboSize;
-
-
-
 };
 
 #endif
