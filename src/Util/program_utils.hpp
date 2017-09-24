73db13d3312701b4fe84ceee98cd2887d84bce8b
Alberto Spina <as12015@ic.ac.uk>
2017-09-24 19:12:08 +0200

    program_utils.hpp: Refactor virtual layout initialisation code and btn handling to Utils

--- src/Util/program_utils.hpp
+++ src/Util/program_utils.hpp
@@ -2,12 +2,24 @@
 #define PROGRAM_UTILS_H
 
 #include "Util.hpp"
+#include "../Util/camera_utils.hpp"
 #include "../Program/Program.hpp"
+#include "../Component/Component.hpp"
+#include "../Component/Line.hpp"
+#include "../Component/Prism.hpp"
+#include "../Component/Texture.hpp"
 
 shared_ptr<Program> initProgram(const char* vs, const char* fs);
 
 void attachUniforms(const char* name, GLfloat* uniform);
 
 void drawAllPrograms();
+
+void updateButtonData(uint8_t* data);
+void updateCompanionCube(float);
+
+void generateModels();
+
+void updateButtons(shared_ptr<Component>, int);
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -20,24 +20,11 @@
 using namespace std;
 
 /* Keeps track of window size for things like the viewport and the mouse cursor */
-int g_gl_width = 640;
-int g_gl_height = 480;
+int g_gl_width = 1280;
+int g_gl_height = 960;
 
 /* Window instance */
 GLFWwindow *g_window = NULL;
-
-shared_ptr<Component> topButton;
-shared_ptr<Component> rightButton;
-shared_ptr<Component> botButton;
-shared_ptr<Component> leftButton;
-
-shared_ptr<Component> targetButton;
-list<shared_ptr<Component>> availableButtons;
-
-shared_ptr<Component> companionCube;
-
-void generateModels();
-void updateButtons(shared_ptr<Component>, int);
 
 int main() {
 	restart_gl_log();
@@ -53,8 +40,6 @@ int main() {
 
 	/* Set up the models in the virtual layout */
 	generateModels();
-
-	targetButton = leftButton;
 
 	/* Initialise the camera instance.
 	   Holds an instance of the view matrix. */
@@ -103,7 +88,7 @@ int main() {
 
 		/* Update the camera based on Key Press events */
 		bool cam_moved = update_camera(g_window, elapsed_seconds);
-		companionCube->setPos(getCubePos(elapsed_seconds));
+		updateCompanionCube(elapsed_seconds);
 
 		/* Update view matrix */
 		if (cam_moved) {
@@ -139,151 +124,10 @@ int main() {
 
 		/* Read the data sent over by arduino */
 		uint8_t* readData = readByteData();
-		if (readData != NULL) {
-			if (sizeof(readData) > 0) {
-				updateButtons(leftButton, readData[3]);
-				updateButtons(botButton, readData[2]);
-				updateButtons(rightButton, readData[1]);
-				updateButtons(topButton, readData[0]);
-			}
-			free (readData);
-		}
-
-		targetButton->updateSubCompTexture(1);
+		updateButtonData(readData);
 	}
 
 	/* Close GL context and any other GLFW resources */
 	glfwTerminate();
 	return 0;
-}
-
-/* -------------------------------------------------------------------------- */
-/* Helper functions */
-
-/* */
-void updateButtons(shared_ptr<Component> btn, int val) {
-	btn->updateSubCompTexture(val*2);
-
-	if (val == 1 && btn == targetButton) {
-		targetButton = availableButtons.front();
-		availableButtons.pop_front();
-		availableButtons.push_back(btn);
-	}
-}
-
-/* */
-void generateModels() {
-	/* Initialise Program for textured Objects */
-	shared_ptr<Program> texturedProgram = initProgram("assets/test_vs.glsl", "assets/test2_fs.glsl");
-
-	/* */
-	shared_ptr<Texture> cubeTexture = make_shared<Texture>();
-	cubeTexture->addTexture("assets/img/companionCube.png");
-
-	/* */
-	shared_ptr<Texture> wallTexture = make_shared<Texture>();
-	wallTexture->addTexture("assets/img/grid3.png");
-	wallTexture->addTexture("assets/img/grid3b.png");
-
-	/* */
-	shared_ptr<Texture> iconTexture = make_shared<Texture>();
-	iconTexture->addTexture("assets/img/Companion-Cube-portal-2.jpg");
-	iconTexture->addTexture("assets/img/Chamber_icon_cake_on.png");
-
-	/* */
-	shared_ptr<Texture> buttonTexture = make_shared<Texture>();
-	buttonTexture->addTexture("assets/img/grid0Gray.png");
-	buttonTexture->addTexture("assets/img/grid0Blue.png");
-	buttonTexture->addTexture("assets/img/grid0Orange.png");
-
-	/* */
-	shared_ptr<Texture> signalTexture = make_shared<Texture>();
-	signalTexture->addTexture("assets/img/portalSignalLightGray.png");
-	signalTexture->addTexture("assets/img/portalSignalLightBlue.png");
-	signalTexture->addTexture("assets/img/portalSignalLightOrange.png");
-
-	/* ----- RIGHT BUTTON GENERATION ----- */
-	texturedProgram->generateTexturedPrism(10, 10, 50, make_shared<Point>(10, 0, -26), wallTexture, 5);
-
-	rightButton = make_shared<Component>();
-
-	/* Actual "button" component */
-	rightButton->addSubComponent(texturedProgram->generateTexturedPrism(5, 5, 2, make_shared<Point>(10, 0, -1.25f), buttonTexture, 1.25f));
-
-	/* Side coloured trail */
-	rightButton->addSubComponent(texturedProgram->generateTexturedPrism(0.5f, 0.5f, 0, make_shared<Point>(6.875f, -0.6f, -0.99f), signalTexture, 0));
-	rightButton->addSubComponent(texturedProgram->generateTexturedPrism(0.5f, 0.5f, 0, make_shared<Point>(5.625f, -0.6f, -0.99f), signalTexture, 0));
-	for (float i = -1; i >= -30; i-= 1.25f) {
-	 	rightButton->addSubComponent(texturedProgram->generateTexturedPrism(0, 0.5f, 0.5f, make_shared<Point>(4.99f, -0.6f,  i - (1.25f/2)), signalTexture, 0));
-	}
-
-	/* ----- TOP BUTTON GENERATION ----- */
-	texturedProgram->generateTexturedPrism(10, 10, 50, make_shared<Point>(0, 10, -26), wallTexture, 5);
-
-	/* Companion Cube icon */
-	texturedProgram->generateTexturedPrism(1.25f, 1.25f, 0.01f, make_shared<Point>(-3.13f, 6.85f, -0.99f), iconTexture, 0);
-
-	/* Cake icon */
-	shared_ptr<Component> cakeIcon = texturedProgram->generateTexturedPrism(1.25f, 1.25f, 0.01f, make_shared<Point>(3.13f, 6.85f, -0.99f), iconTexture, 0);
-	cakeIcon->setTextureIndex(1);
-
-	topButton = make_shared<Component>();
-
-	/* Actual "button" component */
-	topButton->addSubComponent(texturedProgram->generateTexturedPrism(5, 5, 2, make_shared<Point>(0, 10, -1.25f), buttonTexture, 1.25f));
-
-	/* Side coloured trail */
-	topButton->addSubComponent(texturedProgram->generateTexturedPrism(0.5f, 0.5f, 0, make_shared<Point>(-0.6f, 6.875f, -0.99f), signalTexture, 0));
-	topButton->addSubComponent(texturedProgram->generateTexturedPrism(0.5f, 0.5f, 0, make_shared<Point>(-0.6f, 5.625f, -0.99f), signalTexture, 0));
-	for (float i = -1; i >= -30; i-= 1.25f) {
-	 	topButton->addSubComponent(texturedProgram->generateTexturedPrism(0.5f, 0, 0.5f, make_shared<Point>(-0.6f, 4.99f,  i - (1.25f/2)), signalTexture, 0));
-	}
-
-	/* ----- LEFT BUTTON GENERATION ----- */
-	texturedProgram->generateTexturedPrism(10, 10, 50, make_shared<Point>(-10, 0, -26), wallTexture, 5);
-
-	leftButton = make_shared<Component>();
-
-	/* Actual "button" component */
-	leftButton->addSubComponent(texturedProgram->generateTexturedPrism(5, 5, 2, make_shared<Point>(-10, 0, -1.25f), buttonTexture, 1.25f));
-
-	/* Side coloured trail */
-	leftButton->addSubComponent(texturedProgram->generateTexturedPrism(0.5f, 0.5f, 0, make_shared<Point>(-6.875f, -0.6f, -0.99f), signalTexture, 0));
-	leftButton->addSubComponent(texturedProgram->generateTexturedPrism(0.5f, 0.5f, 0, make_shared<Point>(-5.625f, -0.6f, -0.99f), signalTexture, 0));
-	for (float i = -1; i >= -30; i-= 1.25f) {
-	 	leftButton->addSubComponent(texturedProgram->generateTexturedPrism(0, 0.5f, 0.5f, make_shared<Point>(-4.99f, -0.6f,  i - (1.25f/2)), signalTexture, 0));
-	}
-
-	/* ----- BOT BUTTON GENERATION ----- */
-	texturedProgram->generateTexturedPrism(10, 10, 50, make_shared<Point>(0, -10, -26), wallTexture, 5);
-
-	botButton = make_shared<Component>();
-
-	/* Actual "button" component */
-	botButton->addSubComponent(texturedProgram->generateTexturedPrism(5, 5, 2, make_shared<Point>(0, -10, -1.25f), buttonTexture, 1.25f));
-
-	/* Side coloured trail */
-	botButton->addSubComponent(texturedProgram->generateTexturedPrism(0.5f, 0.5f, 0, make_shared<Point>(-0.6f, -6.875f, -0.99f), signalTexture, 0));
-	botButton->addSubComponent(texturedProgram->generateTexturedPrism(0.5f, 0.5f, 0, make_shared<Point>(-0.6f, -5.625f, -0.99f), signalTexture, 0));
-	for (float i = -1; i >= -30; i-= 1.25f) {
-	 	botButton->addSubComponent(texturedProgram->generateTexturedPrism(0.5f, 0, 0.5f, make_shared<Point>(-0.6f, -4.99f,  i - (1.25f/2)), signalTexture, 0));
-	}
-
-	/* Background gray component */
-	shared_ptr<Component> background = texturedProgram->generateTexturedPrism(200, 200, 0, make_shared<Point>(0, 0, -16), wallTexture, 1);
-	background->setTextureIndex(1);
-
-	/* Companion cube */
-	companionCube = texturedProgram->generateTexturedPrism(2, 2, 2, make_shared<Point>(0, 0, 0.75f), cubeTexture, 0);
-
-	/* */
-	availableButtons.push_back(rightButton);
-	availableButtons.push_back(topButton);
-	availableButtons.push_back(botButton);
-
-	/* Initialise Program for blank objects. Templates and/or Grid */
-	shared_ptr<Program> untexturedProgram = initProgram("assets/test2_vs.glsl", "assets/test_fs.glsl");
-
-	/* Grid element */
-	//untexturedProgram->generateGrid(100, 1.0f);
 }
