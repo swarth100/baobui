2907995a2bef77158e7baf28cf38367a9c0fdf74
Alberto Spina <as12015@ic.ac.uk>
2017-09-26 20:43:47 +0200

    program_utils.hpp: Refactor adding thorough commenting fixing style

--- src/Util/program_utils.hpp
+++ src/Util/program_utils.hpp
@@ -1,6 +1,7 @@
 #ifndef PROGRAM_UTILS_H
 #define PROGRAM_UTILS_H
 
+#include <SFML/Audio.hpp>
 #include "Util.hpp"
 #include "../Util/camera_utils.hpp"
 #include "../Program/Program.hpp"
@@ -9,20 +10,35 @@
 #include "../Component/Prism.hpp"
 #include "../Component/Texture.hpp"
 
+/* Program_utils contains a number of helper functions to handle Programs,
+ * graphics and audio.
+ * Program_utils must be queried when initialising programs, attaching uniforms
+ * to them or issuing draw requests. Program_utils stores the only list of
+ * active Program*s.
+ * Program_utils includes stb_image.h and is thus able to load textures and
+ * render them onto the screen. Due to this it also keeps track of all Virtual
+ * components of the scene, and must be queried when updating the companionCube
+ * or during interactions with buttons.
+ * Being the only utility to include SFML/Audio.h, audio files must be imported
+ * and run fro within this utility.
+ */
+
+/* Program initialisation and utility */
 shared_ptr<Program> initProgram(const char* vs, const char* fs);
-
 void attachUniforms(const char* name, GLfloat* uniform);
-
 void drawAllPrograms();
+
+/* Button data initialisation and utility */
 
 void updateButtonData(uint8_t* data);
 void updateCompanionCube(float);
+void updateButtons(shared_ptr<Component>, int);
 
+/* Texturing and Virtual Modelling */
 void generateModels();
 
+/* SOund handling */
 void initSound();
 shared_ptr<sf::Sound> loadSound(const char*, int);
-
-void updateButtons(shared_ptr<Component>, int);
 
 #endif
